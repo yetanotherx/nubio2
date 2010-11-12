4ba004f4efea15c9b7a06282790c182bc9b0517e
Soxred93 <soxred93@gmail.com>
2010-11-12 13:41:25 -0500

    add what I forgot to insert, move private project configuration to PrivateConfiguration

--- web/frontend_dev.php
+++ web/frontend_dev.php
@@ -3,11 +3,13 @@
 define( 'INITTIME', microtime(1) );
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
+require_once(dirname(__FILE__).'/../config/PrivateConfiguration.class.php');
+
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'dev', true);
 
 if (!in_array(@$_SERVER['REMOTE_ADDR'], $allowed_ips))
 {
   die('Hey! Get outta here! This area\'s private, dontchaknow!');
 }
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'dev', true);
 sfContext::createInstance($configuration)->dispatch();
--- web/index.php
+++ web/index.php
@@ -3,6 +3,7 @@
 define( 'INITTIME', microtime(1) );
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
+require_once(dirname(__FILE__).'/../config/PrivateConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
 sfContext::createInstance($configuration)->dispatch();
