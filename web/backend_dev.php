2b86f9ad017d88d3e704fee61820054860498574
Soxred93 <soxred93@gmail.com>
2010-11-12 17:27:54 -0500

    MD5 to protect privacy

--- web/backend_dev.php
+++ web/backend_dev.php
@@ -5,8 +5,7 @@ define( 'INITTIME', microtime(1) );
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 $configuration = ProjectConfiguration::getApplicationConfiguration('backend', 'dev', true);
 
-if (!in_array(@$_SERVER['REMOTE_ADDR'], $allowed_ips))
-{
+if ( !in_array( md5( (string) @$_SERVER['REMOTE_ADDR'] ), $allowed_ips ) ) {
   die('Hey! Get outta here! This area\'s private, dontchaknow!');
 }
 
--- web/frontend_cache.php
+++ web/frontend_cache.php
@@ -6,8 +6,7 @@ require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'cache', true);
 
-if (!in_array(@$_SERVER['REMOTE_ADDR'], $allowed_ips))
-{
+if ( !in_array( md5( (string) @$_SERVER['REMOTE_ADDR'] ), $allowed_ips ) ) {
   die('Hey! Get outta here! This area\'s private, dontchaknow!');
 }
 
